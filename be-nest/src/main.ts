5d44b06eeb371ce0908a35e07b57be9c658903c7
Rizjami Putera <rizjamiputera@gmail.com>
2024-09-03 15:06:17 +0700

    add fe cors

--- be-nest/src/main.ts
+++ be-nest/src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const port = 5003
-  await app.listen(port);
-  console.log(`App is running at http://localhost:${port}`)
+    const app = await NestFactory.create(AppModule);
+
+    // CORS
+    app.enableCors({
+        origin: ['http://localhost:3000', 'http://localhost:3001'],
+        credentials: true
+    });
+
+    const port = 5003
+    await app.listen(port);
+    console.log(`App is running at http://localhost:${port}`)
 }
 bootstrap();
