fcda61d540ddfcef8ac44022b0bcc1ba53664d80
Rizjami Putera <rizjamiputera@gmail.com>
2024-09-03 12:01:50 +0700

    create user resource

---
+++ be-nest/src/users/users.controller.ts
@@ -0,0 +1,51 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    try {
+      return this.usersService.findAll();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    try {
+      return this.usersService.findOne(id);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    try {
+      return this.usersService.update(id, updateUserDto);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id') id: string) {
+    try {
+      return this.usersService.remove(id);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
---
+++ be-nest/src/users/users.interface.ts
@@ -0,0 +1,9 @@
+interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
